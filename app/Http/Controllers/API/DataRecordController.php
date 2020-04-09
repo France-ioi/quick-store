ebfdb331ff7489fb1c07093a96301eafed5b8008
mobydimk <mobydimk@gmail.com>
2020-04-09 06:56:36 +0400

    api request validation

--- app/Http/Controllers/API/DataRecordController.php
+++ app/Http/Controllers/API/DataRecordController.php
@@ -7,12 +7,20 @@ use App\Http\Controllers\Controller;
 use App\User;
 use App\DataRecord;
 use Hash;
+use Validator;
 
 class DataRecordController extends Controller
 {
 
 
     public function read(Request $request) {
+        $validator = $this->validatorRead($request);
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->messages()->getMessages()
+            ]);                    
+        }                
         $user = User::where('prefix', $request->get('prefix'))->first();
         if(!$user) {
             return response()->json([
@@ -35,8 +43,24 @@ class DataRecordController extends Controller
     }
 
 
+    private function validatorRead($request) {
+        return Validator::make($request->all(), [
+            'prefix' => 'required',
+            'key' => 'required|max:'.config('data_record.key_max_length')
+        ]);
+    }        
+
+
 
     public function write(Request $request) {
+        $validator = $this->validatorWrite($request);
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->messages()->getMessages()
+            ]);                    
+        }        
+
         $user = User::where('prefix', $request->get('prefix'))->first();
         if(!$user) {
             return response()->json([
@@ -50,7 +74,6 @@ class DataRecordController extends Controller
                 'message' => 'Wrong password'
             ]);            
         }
-
         $rec = DataRecord::updateOrCreate(
             [
                 'user_id' => $user->id,
@@ -63,6 +86,16 @@ class DataRecordController extends Controller
         return response()->json([
             'success' => true
         ]);        
+    }    
+
+
+    private function validatorWrite($request) {
+        return Validator::make($request->all(), [
+            'prefix' => 'required',
+            'password' => 'required',
+            'key' => 'required|max:'.config('data_record.key_max_length'),
+            'value' => 'required|max:'.config('data_record.value_max_length'),
+        ]);
     }    
 
 }
